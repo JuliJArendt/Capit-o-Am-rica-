8853c31cd8ac4cd134e7977eb70daf2e08048b33
JuliJArendt <163137118+JuliJArendt@users.noreply.github.com>
2024-03-12 09:41:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://static.wikia.nocookie.net/marvelcinematicuniverse/images/4/45/Steven_Rogers.jpg/revision/latest?cb=20200630143146&path-prefix=pt-br";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://pm1.aminoapps.com/6415/3483f16d8431d2335fbd81ee308322675d233cda_00.jpg";
+}
